ccf3d0624570d995954e853609c2c780ad334e0d
Uraki0103 <uraki0103@gmail.com>
2026-08-29 21:20:32 +0900

    add:記録編集画面を作成

--- app/controllers/records_controller.rb
+++ app/controllers/records_controller.rb
@@ -1,5 +1,6 @@
 class RecordsController < ApplicationController
   before_action :authenticate_user!
+  before_action :set_record, only: %i[show edit update]
 
   def index
     @records_by_year = current_user.records.includes(:movie, :theater).order(watched_day: :desc).group_by { |record| record.watched_day.year }
@@ -12,17 +13,9 @@ class RecordsController < ApplicationController
   def create
     @record = current_user.records.build(record_params)
 
-    Rails.logger.debug "===== DEBUG ====="
-    Rails.logger.debug "params[:record]: #{params[:record].inspect}"
-    Rails.logger.debug "movie_title_param: #{movie_title_param.inspect}"
-    Rails.logger.debug "=================="
-
-    if movie_title_param.blank?
-      @record.errors.add(:base, "映画タイトルを入力してください")
+    unless assign_movie(@record)
       return render :new, status: :unprocessable_entity
     end
-
-    @record.movie = Movie.find_or_create_by(title: movie_title_param)
 
     if @record.save
       redirect_to records_path, notice: "記録を保存しました"
@@ -32,10 +25,37 @@ class RecordsController < ApplicationController
   end
 
   def show
-    @record = current_user.records.find(params[:id])
+  end
+
+  def edit
+  end
+
+  def update
+    unless assign_movie(@record)
+      return render :edit, status: :unprocessable_entity
+    end
+
+    if @record.update(record_params)
+      redirect_to records_path, notice: "記録を更新しました"
+    else
+      render :edit, status: :unprocessable_entity
+    end
   end
 
   private
+
+  def assign_movie(record)
+    if movie_title_param.blank?
+      record.errors.add(:base, "映画タイトルを入力してください")
+      return false
+    end
+
+    record.movie = Movie.find_or_create_by(title: movie_title_param)
+  end
+
+  def set_record
+    @record = current_user.records.find(params[:id])
+  end
 
   def movie_title_param
     params.dig(:record, :movie_title)
