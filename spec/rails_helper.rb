3226ae50de05996d33e229fe66657d84516479a3
Uraki0103 <uraki0103@gmail.com>
2026-09-08 22:59:54 +0900

    add:記録関係のシステムスペック、思い出のモデルスペックを追加

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -37,6 +37,21 @@ begin
 rescue ActiveRecord::PendingMigrationError => e
   abort e.to_s.strip
 end
+
+require "capybara/rspec"
+require "selenium-webdriver"
+
+Capybara.register_driver :selenium_chrome_headless_docker do |app|
+  options = Selenium::WebDriver::Chrome::Options.new
+  options.add_argument("--headless=new")
+  options.add_argument("--no-sandbox")
+  options.add_argument("--disable-dev-shm-usage")
+  options.add_argument("--disable-gpu")
+  options.add_argument("--window-size=1400,1400")
+
+  Capybara::Selenium::Driver.new(app, browser: :chrome, options: options)
+end
+
 RSpec.configure do |config|
   config.include FactoryBot::Syntax::Methods
   config.include Devise::Test::IntegrationHelpers, type: :request
@@ -45,6 +60,15 @@ RSpec.configure do |config|
   config.before(:each, type: :system) do
     driven_by :rack_test
   end
+
+  config.before(:each, type: :system) do |example|
+    if example.metadata[:js]
+      driven_by :selenium_chrome_headless_docker
+    else
+      driven_by :rack_test
+    end
+  end
+
   config.fixture_paths = [
     Rails.root.join('spec/fixtures')
   ]
